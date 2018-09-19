42143c9d4a83579c1f54d66dcf4d7c253b239f9d
Dallas Parker <dallasjamesparker@gmail.com>
2018-09-18 20:07:50 -0600

    add 404 page, navbar, and set up routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { Fragment } from 'react';
+import { Route, Switch} from 'react-router-dom';
 import Home from './components/Home';
+import About from './components/About';
+import NoMatch from './components/NoMatch';
+import NavBar from './components/NavBar';
 
 const App = () => (
-   <Route exact path="/" component={Home} />
+  <Fragment>
+    <NavBar />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route component={NoMatch} />
+    </Switch>
+  </Fragment>
 );
 
 export default App;
